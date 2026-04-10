31ed48581d5ee1434b79e844abd6b5fdd7b65c94
Pooja <poojavenkatesan15042001@gmail.com>
2026-04-10 12:21:12 +0530

    Fix: Emergency Vercel Deployment Sync - FORCE-PROD-V5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      {/* PROD SYNC: 2026-04-10-STABLE */}
+    <App />
     </ThemeProvider>
   </React.StrictMode>,
 )
